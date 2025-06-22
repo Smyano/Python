758ff7c9d3dcefb18da40ca7a9cad61743dc54c0
yano <samirayanos2@gmail.com>
2025-06-22 00:18:51 -0300

    calulcar cm e mm de X metros

---
+++ ex08.py
@@ -0,0 +1,4 @@
+D = int(input("Digite o valor de metros para descobrir seus centímetros e milímetros "))
+D1 = D * 100
+D2 = D * 1000
+print (f" {D} metros equivalem a {D1} centímetros e {D2} milímetros")
