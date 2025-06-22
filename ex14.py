5957086f65abd386b84a92095d47676da15b10f5
yano <samirayanos2@gmail.com>
2025-06-22 00:27:58 -0300

    conversor de temperatura °C/°F

---
+++ ex14.py
@@ -0,0 +1,8 @@
+# c = float(input("Digite quantos graus °C está fazendo "))
+# R = (c * 1.8) + 32 
+# print(f"A temperatura convetida em será {R:.2f}°F")
+
+F = float(input("Digite quantos graus °F está fazendo "))
+Re = (F - 32) / 1.8 
+print(f"A temperatura convetida em será {Re:.2f}°C")
+
