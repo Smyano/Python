edc3f340b793f373214c27bac361a671dac83433
yano <samirayanos2@gmail.com>
2025-06-22 00:07:23 -0300

    Anotações

---
+++ anot.py
@@ -0,0 +1,40 @@
+
+# ANOTAÇÕES
+# nome = (input("Digite seu nome: "))
+# idade = (input("Digite sua idade: "))
+# print(f"Nome: {nome} e Idade: {idade} anos")
+
+# print(f"Nome: {nome:^10}")
+
+
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))    
+# soma = n1 + n2
+# print(f"A soma de {n1} e {n2} é: {soma}")
+
+# nome = input("Digite seu nome: ")
+# print (f"olá" , nome)
+
+# dia = input("Digite o dia do seu nascimento: ")
+# mes = input("Digite o mes do seu nascimento: ")
+# ano = input("Digite o ano do seu nascimento: ")
+# print(f"Você nasceu no dia {dia} de {mes} de {ano}")
+
+# n1 = int(input("Digite um número:"))
+# n2 = int(input("Digite um outro:"))
+# soma = n1 + n2
+# print(f"A soma dos núnmeros digitados são: {soma}") 
+# == igual
+
+# tipos primitivos: int, string, boolean, float
+
+# operadores aritméticos
+# ** potência, // divisão interia, %Rdivisão
+
+# ordem de precendência
+# 1° (); 
+# 2° ** "** > potência"; **1/2> raiz quadrada 
+# 3° *, /, //, %;
+# 4° +, -;
+
+
