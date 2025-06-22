f323125000297118d236bc4e4110e8b3f762be65
yano <samirayanos2@gmail.com>
2025-06-22 00:24:50 -0300

    calcular m²

---
+++ ex11.py
@@ -0,0 +1,6 @@
+n = float(input("Digite a largura da sua parede "))
+n1 = float(input("Digite a altura da sua parede "))
+a = n * n1
+q = a / 2
+ 
+print(f"A largura são {n} e altura são {n1}, sendo {a}m² de área, sabendo que 1 litro de tinta pinta 2m², a quantidade de tinta necessária será {q} litros de tinta")
