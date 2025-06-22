b009304263e4db6abe883a117f5149fd35df8121
yano <samirayanos2@gmail.com>
2025-06-22 00:50:36 -0300

    calcular o dobro, triplo e a raiz quadrada de um valor determido pelo usuário

--- ex06.py
+++ ex06.py
@@ -2,5 +2,5 @@ n = int(input("Digite um número "))
 n1 = n * 2
 n2 = n * 3
 # n3 = n * n
-r = n ** (1/2)
-print(f"O dobro do número digitado é {n1} \n o triplo do número desse número é {n2} \n e a raiz quadrada desse número é {r}")
+n3 = n ** (1/2)
+print(f"O dobro do número digitado é {n1} \n o triplo do número desse número é {n2} \n e a raiz quadrada desse número é {n3}")
